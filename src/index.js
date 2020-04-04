75f66dc07ccf52173ae43e53b1b9ad44874f8ace
Rodrigo Burgos <rodrigoburgoscl@gmail.com>
2020-04-04 20:15:19 -0300

    Iniciando proyecto

---
+++ src/index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+// Settings
+app.set('port', 3000)
+
+// Middlewares
+
+// Routes
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views/index.html'));
+})
+
+// Static files
+
+// Listening the server
+app.listen(app.get('port'), () => {
+    console.log('Server on port', app.get('port'));
+});
