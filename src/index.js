e6625447b7320b4205229b6cf6002aafb0ba56f8
Rodrigo Burgos <rodrigoburgoscl@gmail.com>
2020-04-04 20:50:21 -0400

    Implemento plantilla de html

--- src/index.js
+++ src/index.js
@@ -3,14 +3,16 @@ const app = express();
 const path = require('path');
 
 // Settings
-app.set('port', 3000)
+app.set('port', 3000);
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
 
 // Middlewares
 
 // Routes
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'views/index.html'));
-})
+    res.render('index', {title: 'First Website'});
+});
 
 // Static files
 
